09f9fa5e28f39ba433b25b639a8f5c434f588e96
harley444444444 <48341558+harley444444444@users.noreply.github.com>
2019-04-12 08:36:24 +0800

    Add files via upload

---
+++ HW1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+printf("hello world   3a713202");
+	
+	
+	
+system("pause");
+return 0;
+	
+} 
