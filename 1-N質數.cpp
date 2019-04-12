0bb967d3dbce5c296847a555d2ff01b78b9ba7b5
harley444444444 <48341558+harley444444444@users.noreply.github.com>
2019-04-12 08:36:01 +0800

    Add files via upload

---
+++ 1-N質數.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h> 
+#include<conio.h> 
+
+
+int main() 
+{ 
+
+int i,j,n,m=0; 
+
+printf("輸入 n ，以便求 1 ~ n 內的質數 ： "); 
+scanf("%d",&n); 
+
+for ( i = 1 ; i <= n ; i++) 
+{ 
+m =0; 
+
+for ( j = 1 ; j <= n ; j++) 
+{ 
+if( (i % j) == 0 ) m++; 
+if ( m > 2 ) break; 
+} 
+if ( m <= 2 ) printf("%d \t 是質數 !\n",i); 
+
+} 
+getch(); 
+return 0; 
+
+} 
