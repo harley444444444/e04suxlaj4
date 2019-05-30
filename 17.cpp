5a874342b27a5ccffded8d3687b43409a9b89b8e
harley444444444 <48341558+harley444444444@users.noreply.github.com>
2019-05-31 00:35:13 +0800

    Add files via upload

---
+++ 17.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int a[5]={0};
+	int i=0,min;
+	printf("請輸入五個數值\n");
+	for(i=0;i<5;i++){
+		printf("請輸入第%d個資料:",i+1);
+		scanf("%d",&a[i]);
+	}
+	min=a[0];
+	for(i=0;i<5;i++){
+		if(min>a[i]){
+			min=a[i];
+		}
+	}
+	printf("最小值:%d\n\n",min);
+	system("PAUSE");
+	return 0;
+}
